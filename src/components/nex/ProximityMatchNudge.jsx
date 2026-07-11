4cf42fe334cb4841f5a865f1ea6faff95b5fd96a
Mostlit <mostlit.mostlit@gmail.com>
2026-07-11 06:00:54 +0000

    File changes

--- src/components/nex/ProximityMatchNudge.jsx
+++ src/components/nex/ProximityMatchNudge.jsx
@@ -84,7 +84,7 @@ export default function ProximityMatchNudge() {
           animate={{ opacity: 1, y: 0, scale: 1 }}
           exit={{ opacity: 0, y: -20, scale: 0.95 }}
           transition={{ duration: 0.35, ease: [0.22, 1, 0.36, 1] }}
-          className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-[92%] max-w-md px-2"
+          className="fixed top-4 left-1/2 -translate-x-1/2 z-[65] w-[92%] max-w-md pl-2 pr-16"
         >
           <motion.button
             onClick={handleTap}
