58730ee97d8de390d8be83479094a4131eaa8329
Mostlit <mostlit.mostlit@gmail.com>
2026-07-11 05:10:26 +0000

    File changes

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -9,7 +9,7 @@ const ALL_INTERESTS = [
   "Technology", "Fitness", "Business", "Cars", "Nightlife", "Photography",
   "Travel", "Food", "Creators", "Startups", "Sports", "Music",
   "Art", "Gaming", "Fashion", "Movies", "Reading", "Hiking",
-  "Yoga", "Cooking", "Design", "Crypto", "Science", "Pets"
+  "Yoga", "Cooking", "Design", "Crypto", "Science", "Pets", "420"
 ];
 
 const VISIBILITY_OPTIONS = [
